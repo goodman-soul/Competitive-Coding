89ffb0324f8f2d659fc858bd82cf9b9a1efcba16
abhinabacr4@gmail.com <abhinabacr4@gmail.com>
2020-11-09 12:59:24 +0530

    training

--- training/Codeforces/CF766-D2-A.cpp
+++ training/Codeforces/CF766-D2-A.cpp
@@ -1,11 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 void solve(){
-
+	string s,x;
+	cin>>s>>x;
+	if(s==x)cout<<"-1"<<endl;
+	else{
+		cout<<max(s.size(),x.size())<<endl;
+	}
+}
 int main()
 {
-	int test;
-	cin>>test;
+	int test=1;
+	//cin>>test;
 	while(test--){
 		solve();
 	}
