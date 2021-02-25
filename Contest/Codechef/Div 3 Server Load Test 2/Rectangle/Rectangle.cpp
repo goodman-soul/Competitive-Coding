39e47ef294234bd19dc4e52b6a596b244a17db37
goodman-soul <abhinabacr4@gmail.com>
2021-02-25 14:51:10 +0530

    Div3 Server load test 2

--- Contest/Codechef/Div 3 Server Load Test 2/Rectangle/Rectangle.cpp
+++ Contest/Codechef/Div 3 Server Load Test 2/Rectangle/Rectangle.cpp
@@ -1,13 +1,51 @@
-#include<bits/stdc++.h> 
+#include<bits/stdc++.h>
 using namespace std;
-void solve(){
-	
-}
 int main()
 {
-	int test=1;
-	scanf("%d",&test);
-	for(int i=1;i<=test;i++){
-	solve();
+	int test;
+	cin>>test;
+	while(test--){
+		int n,m;
+		cin>>n>>m;
+		int x=-1,y=-1,a=-1,b=-1;
+		int arr[n][m];
+		for(int i=0;i<n;i++){
+			string s;
+			cin>>s;
+			for(int j=0;j<m;j++){
+				if(s[j]=='1'){
+					arr[i][j]=1;
+				}else{
+					arr[i][j]=0;
+				}
+			}
+		}
+		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				if(arr[i][j]==1){
+					if(x==-1){
+						x=i;y=i;
+					}else{
+						y=i;
+					}
+					if(a==-1){
+						a=j;
+						b=j;
+					}else{
+						a=min(a,j);
+						b=max(b,j);
+					}
+				}
+			}
+		}
+		string ans="YES";
+		for(int i=x;i<=y;i++){
+			for(int j=a;j<=b;j++){
+				if(arr[i][j]==0){
+					ans="NO";
+				}
+			}
+		}
+		cout<<ans<<endl;
 	}
 }
