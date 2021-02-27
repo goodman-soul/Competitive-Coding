80baae16ac24618b06af0d365d8c743465dc7086
goodman-soul <abhinabacr4@gmail.com>
2021-02-28 01:12:31 +0530

    Feb Lunchtime 2021

--- Contest/Codechef/Feb Lunch Time Div3/Average Sorting/Average Sorting.cpp
+++ Contest/Codechef/Feb Lunch Time Div3/Average Sorting/Average Sorting.cpp
@@ -1,13 +1,29 @@
-#include<bits/stdc++.h> 
+#include<bits/stdc++.h>
 using namespace std;
-void solve(){
-	
-}
+using ll=long long;
 int main()
 {
-	int test=1;
-	scanf("%d",&test);
-	for(int i=1;i<=test;i++){
-	solve();
+	auto foo=[](vector<ll> v)->bool{
+		for(int i=1;i<(int)v.size();i++){
+			if(v[i-1]<v[i]){
+				return false;
+			}
+		}
+		return true;
+	};
+	int test;
+	cin>>test;
+	while(test--){
+		int n;
+		cin>>n;
+		vector<ll> v(n);
+		for(int i=0;i<n;i++){
+			cin>>v[i];
+		}
+		if(foo(v)){
+			cout<<"NO"<<endl;
+		}else{
+			cout<<"YES"<<endl;
+		}
 	}
 }
